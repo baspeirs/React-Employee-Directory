e5a1a99716f8608364d2d0cfa378ab56fd965c38
Bryce Speirs <baspeirs@gmail.com>
2020-08-04 16:59:02 -0400

    Created json file with default employees and able to render employee list through components to display cards on page for each employee

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
-import Employee from "./components/Employee"
+import employees from "./employees.json"
+import Card from './components/Card';
+import Container from "./components/Container"
+import Row from "./components/Row"
+import Column from "./components/Column"
 
 class App extends React.Component {
+  state = {
+    employees
+  }
   render() {
     return (
-      <Employee />
+      <Container style={{ marginTop: 100 }}>
+      <Row>
+          <Column size="md-12">
+              <h1>Hello World</h1>
+              {this.state.employees.map(employee => (
+                <Card 
+                firstName={employee.firstName}
+                lastName={employee.lastName}
+                title={employee.title}
+                id={employee.id}
+                />
+              ))}
+          </Column>
+      </Row>
+  </Container>
     );
   };
 };
