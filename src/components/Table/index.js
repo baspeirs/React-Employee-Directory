2afda3d4b1fd4b32e83e76ff4161712c6b402210
Bryce Speirs <baspeirs@gmail.com>
2020-08-07 22:15:40 -0400

    moved all folders/files out of client folder

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,7 +6,7 @@ function Table(props) {
             <table className="table table-dark">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
+                        <th scope="col">ID #</th>
                         <th scope="col">First <img src="https://img.icons8.com/windows/32/000000/circled-chevron-up.png" alt="arrow up" onClick={props.sortFirstASC}/><img src="https://img.icons8.com/windows/32/000000/circled-chevron-down.png" alt="arrow up" onClick={props.sortFirstDEC}/></th>
                         <th scope="col">Last <img src="https://img.icons8.com/windows/32/000000/circled-chevron-up.png" alt="arrow up" onClick={props.sortLastASC}/><img src="https://img.icons8.com/windows/32/000000/circled-chevron-down.png" alt="arrow up" onClick={props.sortLastDEC}/></th>
                         <th scope="col">Title </th>
