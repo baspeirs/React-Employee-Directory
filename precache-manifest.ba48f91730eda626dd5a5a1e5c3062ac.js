76e93afa0a961aaa28bb55e2f68d5b9b74bfe65a
Bryce Speirs <baspeirs@gmail.com>
2020-08-07 22:20:02 -0400

    Updates

---
+++ precache-manifest.ba48f91730eda626dd5a5a1e5c3062ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe0c085f5d4e95fec1d6d9ef401d807",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "1992f5c18dbcddab7ea4",
+    "url": "/React-Employee-Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16e62d8ca783f0fc6ea0",
+    "url": "/React-Employee-Directory/static/js/2.df65fbd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.df65fbd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1992f5c18dbcddab7ea4",
+    "url": "/React-Employee-Directory/static/js/main.ea08e596.chunk.js"
+  },
+  {
+    "revision": "0d1f0a4677134e8e2c5a",
+    "url": "/React-Employee-Directory/static/js/runtime-main.3f5d6a7e.js"
+  }
+]);
